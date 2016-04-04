4a4f6f7aa47802d8c13e9fa1586ab02647dde689
alonso240291 <alonso240291@gmail.com>
2016-04-03 21:44:38 -0600

    adding styles for search bar

--- songsApp/js/songsApp.js
+++ songsApp/js/songsApp.js
@@ -22,4 +22,9 @@ angular.module('songsApp', [])
     {   
         $compileProvider.aHrefSanitizationWhitelist(/^\s*(spotify):/);
     }
-]);
+])
+.filter('capitalize', function() {
+  return function(input) {
+      return input.charAt(0).toUpperCase() + input.slice(1);
+   }
+});
